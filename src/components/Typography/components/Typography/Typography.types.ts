430b7f8ceed0a53cbda857c40338f24cf0740b1f
Hassan Chowdhury <hassan.chowdhury1@gmail.com>
2022-01-08 01:04:20 +0000

    feat(typography): add support for h3

--- src/components/Typography/components/Typography/Typography.types.ts
+++ src/components/Typography/components/Typography/Typography.types.ts
@@ -1,6 +1,10 @@
 import { ReactNode } from 'react';
 
-export type ComponentTypes = 'p' | 'h1' | 'h2';
+export type ComponentTypes =
+  | 'p'
+  | 'h1'
+  | 'h2'
+  | 'h3';
 
 export type TypographyProps = {
   as?: ComponentTypes;
