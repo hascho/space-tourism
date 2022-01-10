4dac6f3d402b77da2f4fe204fb025fd18ab5e2b0
Hassan Chowdhury <hassan.chowdhury1@gmail.com>
2022-01-10 16:49:44 +0000

    feat(destination-page): add mobile layout

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -14,32 +14,40 @@ const Destination: NextPage = () => {
       <SkipToContentButton />
       <PrimaryHeader />
 
-      <main id="main" className={classNames("grid-container", styles.gridContainer)}>
-        <NumberedTitle number='01' text='Pick your destination' />
-        <Image
+      <main id="main" className={classNames("grid-container", styles.gridContainer, 'flow')}>
+        <NumberedTitle className={styles.numberedTitle} number='01' text='Pick your destination' />
+        {/* <div className={styles.image}>
+          <Image
+            src="/assets/destination/image-moon.png"
+            alt="the moon"
+            layout="fixed"
+            width="445"
+            height="445"
+          />
+        </div> */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
           src="/assets/destination/image-moon.png"
           alt="the moon"
-          layout="fixed"
-          width="445"
-          height="445"
+          className={styles.image}
         />
 
-        <Tabs>
+        <Tabs className={styles.tabs}>
           <Tab selected>Moon</Tab>
           <Tab selected={false}>Mars</Tab>
           <Tab selected={false}>Europa</Tab>
           <Tab selected={false}>Titan</Tab>
         </Tabs>
 
-        <article>
-          <Typography as='h2'>Moon</Typography>
+        <article className={styles.destinationInfo}>
+          <Typography as='h2' className="fs-800 uppercase ff-serif">Moon</Typography>
           <Typography>
             See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
             regain perspective and come back refreshed. While you’re there, take in some history 
             by visiting the Luna 2 and Apollo 11 landing sites.
           </Typography>
 
-          <div className='flex'>
+          <div className={classNames(styles.destinationMeta, 'flex')}>
             <div>
               <Typography as='h3' className='text-accent fs-200 uppercase'>Avg. distance</Typography>
               <Typography className='fs-500 ff-serif uppercase'>384,400 km</Typography>
