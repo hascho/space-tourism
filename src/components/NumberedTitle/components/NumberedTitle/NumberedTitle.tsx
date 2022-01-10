56442c92e9a7566cb79e81c33e3f9cc8a45d441b
Hassan Chowdhury <hassan.chowdhury1@gmail.com>
2022-01-10 16:46:45 +0000

    feat(numbered-title): add support for className prop

--- src/components/NumberedTitle/components/NumberedTitle/NumberedTitle.tsx
+++ src/components/NumberedTitle/components/NumberedTitle/NumberedTitle.tsx
@@ -1,12 +1,13 @@
+import classNames from 'classnames';
 import styles from './NumberedTitle.module.css';
 import { Typography } from '../../../Typography';
 import { NumberedTitleProps } from './NumberedTitle.types';
 
 export const NumberedTitle = (props: NumberedTitleProps) => {
-  const { number, text, as: component = "h1" } = props;
+  const { number, text, as: component = "h1", className } = props;
 
   return (
-    <Typography as={component} className={styles.numberedTitle}>
+    <Typography as={component} className={classNames(styles.numberedTitle, className)}>
       <span aria-hidden>{number}</span>{' '}{text}
     </Typography>
   );
--- src/components/NumberedTitle/components/NumberedTitle/NumberedTitle.types.ts
+++ src/components/NumberedTitle/components/NumberedTitle/NumberedTitle.types.ts
@@ -4,4 +4,5 @@ export interface NumberedTitleProps {
   number: string;
   text: string;
   as?: Exclude<TypographyTypes, "p">;
+  className?: string;
 }
